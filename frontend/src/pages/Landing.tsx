7c79c87bf12c98c0e74bdc9aa7be811866212bc3
William <wutwilliam@gmail.com>
2025-04-26 01:29:39 -0700

    landing page done

---
+++ frontend/src/pages/Landing.tsx
@@ -0,0 +1,20 @@
+import "./Landing.css";
+
+function Landing() {
+  return (
+    <div className="body">
+      <h1 className="title">RepoReleaf</h1>
+      <p>
+        Very cool description here that outlines the functionality and purpose
+      </p>
+      <label>
+        Enter Github Repo Link:
+        <input name="githuburl" />
+        {/* <span> </span> */}
+        <button>Generate Report</button>
+      </label>
+    </div>
+  );
+}
+
+export default Landing;
